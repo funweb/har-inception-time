4224b91869998c24d0a23df69b581c1f41e4869f
antOS <943164605@qq.com>
2022-02-07 20:32:46 +0800

    uodate

--- tools/general.py
+++ tools/general.py
@@ -56,5 +56,51 @@ def reTrain(p):
         shutil.rmtree(p)
 
 
+import urllib, json, os, ipykernel, ntpath
+from notebook import notebookapp as app
+
+
+def lab_or_notebook():
+    length = len(list(app.list_running_servers()))
+    if length:
+        return "notebook"
+    else:
+        return "lab"
+
+
+def ipy_nb_name(token):
+    """ Returns the short name of the notebook w/o .ipynb
+        or get a FileNotFoundError exception if it cannot be determined
+        NOTE: works only when the security is token-based or there is also no password
+    """
+
+    if lab_or_notebook() == "lab":
+        from jupyter_server import serverapp as app
+    else:
+        from notebook import notebookapp as app
+    #         from jupyter_server import serverapp as app
+
+    connection_file = os.path.basename(ipykernel.get_connection_file())
+    kernel_id = connection_file.split('-', 1)[1].split('.')[0]
+
+    #     from notebook import notebookapp as app
+    for srv in app.list_running_servers():
+        try:
+            srv['token'] = token
+            if srv['token'] == '' and not srv['password']:  # No token and no password, ahem...
+                req = urllib.request.urlopen(srv['url'] + 'api/sessions')
+                print('no token or password')
+            else:
+                req = urllib.request.urlopen(srv['url'] + 'api/sessions?token=' + srv['token'])
+            sessions = json.load(req)
+            for sess in sessions:
+                if sess['kernel']['id'] == kernel_id:
+                    nb_path = sess['notebook']['path']
+                    return ntpath.basename(nb_path).replace('.ipynb', '')  # handles any OS
+        except:
+            pass  # There may be stale entries in the runtime directory
+    raise FileNotFoundError("Can't identify the notebook name")
+
+
 if __name__ == '__main__':
     print(load_config("configure/config.yaml"))
