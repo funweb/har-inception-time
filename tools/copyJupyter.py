146d4093020eb184a438d6e5b5e4d25894f99cc2
antOS <943164605@qq.com>
2022-02-22 12:35:32 +0800

    update

--- tools/copyJupyter.py
+++ tools/copyJupyter.py
@@ -22,7 +22,7 @@ copy_num = 5
 dataname_list = ["cairo", "milan", "kyoto7", "kyoto8", "kyoto11"]
 
 source_file = os.path.join(op_dir, source_file_name)
-
+target_file_name_list = []
 for i in range(copy_num):
     tag += 1
     dataname = dataname_list[i]
@@ -33,5 +33,8 @@ for i in range(copy_num):
     assert not os.path.exists(target_file), "文件已存在, 请查验"
 
     print("{}->\n{}\n\n".format(source_file, target_file))
+    target_file_name_list.append(target_file)
     time.sleep(3)
     shutil.copy(source_file, target_file)
+
+print(target_file_name_list)
